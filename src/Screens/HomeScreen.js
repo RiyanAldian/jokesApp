be7aff4a1a6f14080c78b61a74df1f6cd27ffaeb
Riyan Aldiansyah <riyanaldiansyah37@gmail.com>
2025-03-03 22:07:15 +0700

    Add ListAccordion

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,9 +1,12 @@
-import {RefreshControl, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {RefreshControl, ScrollView, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import {ListItem, Text} from '@rneui/themed';
+import {Icon, Button} from '@rneui/base';
 
 const HomeScreen = () => {
   const [refreshing, setRefreshing] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
@@ -14,28 +17,71 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <ScrollView
-          contentContainerStyle={styles.scrollView}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-          }>
-          <Text>Pull down to see RefreshControl indicator</Text>
-          <Text>Pull down to see RefreshControl indicator</Text>
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.h1Style}>JokesApp</Text>
+          <ListItem.Accordion
+            content={
+              <>
+                {/* <Icon name="place" size={30} /> */}
+                <Text style={styles.numbering}>1. </Text>
 
-          <Text>Pull down to see RefreshControl indicator</Text>
-
-          <Text>Pull down to see RefreshControl indicator</Text>
-          <Text>Pull down to see RefreshControl indicator</Text>
-
-          <Text>Pull down to see RefreshControl indicator</Text>
-          <Text>Pull down to see RefreshControl indicator</Text>
-          <Text>Pull down to see RefreshControl indicator</Text>
-          <Text>Pull down to see RefreshControl indicator</Text>
-          <Text>Pull down to see RefreshControl indicator</Text>
-          <Text>Pull down to see RefreshControl indicator</Text>
-        </ScrollView>
-      </SafeAreaView>
+                <ListItem.Content>
+                  <ListItem.Title>List Accordion</ListItem.Title>
+                </ListItem.Content>
+                <Button
+                  title="TOP"
+                  loading={false}
+                  loadingProps={{size: 'small', color: 'white'}}
+                  buttonStyle={{
+                    backgroundColor: 'rgba(111, 202, 186, 1)',
+                    borderRadius: 5,
+                  }}
+                  titleStyle={{fontWeight: 'bold', fontSize: 16}}
+                  containerStyle={{
+                    marginHorizontal: 50,
+                    height: 40,
+                    width: 80,
+                    marginVertical: 10,
+                  }}
+                  onPress={() => console.log('aye')}
+                />
+              </>
+            }
+            isExpanded={expanded}
+            onPress={() => {
+              setExpanded(!expanded);
+            }}>
+            {/* {list2.map((l, i) => (
+              <ListItem key={i} onPress={log} bottomDivider>
+                <Avatar title={l.name[0]} source={{uri: l.avatar_url}} />
+                <ListItem.Content>
+                  <ListItem.Title>{l.name}</ListItem.Title>
+                  <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+                </ListItem.Content>
+                <ListItem.Chevron />
+              </ListItem>
+            ))}
+             */}
+            <ListItem>
+              <ListItem.Content>
+                <ListItem.Title>Item 1</ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+            <ListItem>
+              <ListItem.Content>
+                <ListItem.Title>Item 2</ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+          </ListItem.Accordion>
+        </SafeAreaView>
+      </ScrollView>
     </SafeAreaProvider>
   );
 };
@@ -45,11 +91,24 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 10,
   },
-  scrollView: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
+  h1Style: {
+    fontFamily: 'Nunito-SemiBold',
+    fontWeight: '600',
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 10,
   },
+  numbering: {
+    fontFamily: 'Nunito-SemiBold',
+    fontWeight: '1000',
+    fontSize: 18,
+  },
+  //   scrollView: {
+  //     flex: 1,
+  //     backgroundColor: 'pink',
+  //     alignItems: 'center',
+  //     justifyContent: 'center',
+  //   },
 });
