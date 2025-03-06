98da6bbd750b60a8391d37a9b83afd4066b5f2f1
Riyan Aldiansyah <riyanaldiansyah37@gmail.com>
2025-03-06 21:04:04 +0700

    Done

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,30 +1,27 @@
+/* eslint-disable react/no-unstable-nested-components */
 import {
   RefreshControl,
   ScrollView,
   StyleSheet,
-  View,
-  Modal,
   TouchableOpacity,
   Alert,
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import {ListItem, Text} from '@rneui/themed';
-import {Icon, Button} from '@rneui/base';
-// import Modal from 'react-native-modal';
+import {Button} from '@rneui/base';
 
 const HomeScreen = () => {
   const [refreshing, setRefreshing] = React.useState(false);
   const [categories, setCategories] = React.useState([]);
+  const [top, setTop] = React.useState();
+  const [amount, setAmount] = React.useState([]);
   const [jokes, setJokes] = React.useState([]);
-
   const [expanded, setExpanded] = useState([]);
 
-  const [isModalVisible, setisModalVisible] = useState(false);
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const showModal = () => setModalVisible(true);
-  const hideModal = () => setModalVisible(false);
+  useEffect(() => {
+    getCategories();
+  }, []);
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
@@ -34,23 +31,41 @@ const HomeScreen = () => {
     }, 2000);
   }, []);
 
-  const setCetegoryStatus = (title, status) => {
-    const newExpanded1 = expanded.find(item => item.title === title)?.status;
+  const setCetegoryStatus = (title, long, addStatus = false) => {
     const newExpanded = expanded.map(item => {
       if (item.title === title) {
         return {
           ...item,
           status: !item.status ? true : false,
         };
+      } else {
+        return {
+          ...item,
+          status: false,
+        };
       }
-      return item;
     });
-    setExpanded(newExpanded);
+    const expandedStatus = newExpanded.find(
+      item => item.title === title,
+    )?.status;
+
+    if (addStatus === true) {
+      getJokes(title, long);
+    } else {
+      if (expandedStatus === true) {
+        getJokes(title, long);
+        setTimeout(() => {
+          setExpanded(newExpanded);
+        }, 500);
+      } else {
+        console.log('else');
+        setExpanded(newExpanded);
+      }
+    }
   };
 
   const getCetegoryStatus = title => {
     const newExpanded = expanded.find(item => item.title === title)?.status;
-    console.log(newExpanded, 'getCetegoryStatus');
     return newExpanded;
   };
 
@@ -59,23 +74,33 @@ const HomeScreen = () => {
       const response = await fetch('https://v2.jokeapi.dev/categories');
       const json = await response.json();
       const expand = [];
+      const data = [];
       for (let i = 0; i < json.categories.length; i++) {
         expand.push({
           title: json.categories[i],
           status: false,
         });
+        data.push({
+          title: json.categories[i],
+          amount: 2,
+        });
       }
+      setTop(json.categories[0]);
       setExpanded(expand);
       setCategories(json.categories);
+      setAmount(data);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const getJokes = async category => {
+  const getJokes = async (category, amount) => {
     try {
       const response = await fetch(
-        'https://v2.jokeapi.dev/joke/' + category + '?type=single&amount=2',
+        'https://v2.jokeapi.dev/joke/' +
+          category +
+          '?type=single&amount=' +
+          amount,
       );
       const json = await response.json();
       const list = [];
@@ -88,30 +113,35 @@ const HomeScreen = () => {
     }
   };
 
-  const CustomModal = ({visible, onClose}) => {
-    return (
-      <Modal transparent={true} visible={visible} onRequestClose={onClose}>
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Text>Ini Modal!</Text>
-            <Button title="Tutup" onPress={onClose} />
-          </View>
-        </View>
-      </Modal>
-    );
+  const addMoreData = async (title, panjangAmount) => {
+    const newAmount = amount.map(item => {
+      if (item.title === title) {
+        return {
+          ...item,
+          amount: item.amount + 2,
+        };
+      }
+      return item;
+    });
+    const dataAmount = newAmount.find(item => item.title === title)?.amount;
+    console.log(newAmount, 'newAmount');
+    setAmount(newAmount);
+    setCetegoryStatus(title, dataAmount, true);
   };
 
-  const addMoreData = async () => {
-    console.log('Add more data');
-    console.log(categories, 'categories');
+  const GoTop = title => {
+    var category = categories;
+    var index = category.indexOf(title);
+    if (index > -1) {
+      category.splice(index, 1);
+      category.unshift(title);
+    }
+    setCategories(category);
+    setTop(title);
   };
-
-  useEffect(() => {
-    getCategories();
-  }, []);
 
   const modalChild = data => {
-    Alert.alert(data);
+    Alert.alert('', data, [{text: 'OK'}]);
   };
 
   return (
@@ -131,36 +161,59 @@ const HomeScreen = () => {
               content={
                 <>
                   <Text style={styles.numbering}>{i + 1} </Text>
-
                   <ListItem.Content>
-                    <ListItem.Title> {data}</ListItem.Title>
+                    <ListItem.Title>{data}</ListItem.Title>
                   </ListItem.Content>
+                  {top === data && (
+                    <Button
+                      title="TOP"
+                      loading={false}
+                      loadingProps={{size: 'small', color: 'white'}}
+                      buttonStyle={{
+                        backgroundColor: 'rgba(111, 202, 186, 1)',
+                        borderRadius: 5,
+                      }}
+                      titleStyle={{fontWeight: 'bold', fontSize: 16}}
+                      containerStyle={{
+                        marginHorizontal: 50,
+                        height: 40,
+                        width: 80,
+                        marginVertical: 10,
+                      }}
+                    />
+                  )}
 
-                  <Button
-                    title="TOP"
-                    loading={false}
-                    loadingProps={{size: 'small', color: 'white'}}
-                    buttonStyle={{
-                      backgroundColor: 'rgba(111, 202, 186, 1)',
-                      borderRadius: 5,
-                    }}
-                    titleStyle={{fontWeight: 'bold', fontSize: 16}}
-                    containerStyle={{
-                      marginHorizontal: 50,
-                      height: 40,
-                      width: 80,
-                      marginVertical: 10,
-                    }}
-                    onPress={() => {
-                      console.log('aye');
-                    }}
-                  />
+                  {top !== data && (
+                    <Button
+                      title="Go Top"
+                      loading={false}
+                      loadingProps={{size: 'small', color: 'white'}}
+                      buttonStyle={{
+                        backgroundColor: 'rgb(235, 80, 60)',
+                        borderRadius: 5,
+                      }}
+                      titleStyle={{fontWeight: 'bold', fontSize: 16}}
+                      containerStyle={{
+                        marginHorizontal: 50,
+                        height: 40,
+                        width: 80,
+                        marginVertical: 10,
+                      }}
+                      onPress={() => {
+                        GoTop(data);
+                      }}
+                    />
+                  )}
                 </>
               }
               isExpanded={getCetegoryStatus(data)}
               onPress={() => {
-                setCetegoryStatus(data);
-                getJokes(data);
+                const length = amount.find(item => {
+                  if (item.title === data) {
+                    return item.amount;
+                  }
+                });
+                setCetegoryStatus(data, length.amount);
               }}>
               {jokes.map((l, i) => (
                 <ListItem
@@ -183,20 +236,26 @@ const HomeScreen = () => {
                 <TouchableOpacity
                   style={[styles.button]}
                   onPress={() => {
-                    addMoreData();
+                    const length = amount.find(item => {
+                      if (item.title === data) {
+                        return item.amount;
+                      }
+                    });
+                    addMoreData(data, length.amount);
                   }}>
-                  <ListItem.Content style={styles.addMore}>
-                    <ListItem.Title style={styles.addMoreText}>
-                      Add More data
-                    </ListItem.Title>
-                  </ListItem.Content>
+                  {amount.find(item => item.title === data)?.amount !== 6 && (
+                    <ListItem.Content style={styles.addMore}>
+                      <ListItem.Title style={styles.addMoreText}>
+                        Add More data
+                      </ListItem.Title>
+                    </ListItem.Content>
+                  )}
                 </TouchableOpacity>
               </ListItem>
             </ListItem.Accordion>
           ))}
         </SafeAreaView>
       </ScrollView>
-      <CustomModal visible={modalVisible} onClose={hideModal} />
     </SafeAreaProvider>
   );
 };
